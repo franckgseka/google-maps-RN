05ed31e5be39c1582ba8013d7199008e3d36c1d5
franckgseka <franckgseka@gmail.com>
2022-11-26 20:48:07 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import *as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_KEY } from '@env';
 
 export default function App() {
+
+  const [origin, setOrigin] = React.useState ({
+    latitude: 33.640411,
+    longitude: -84.419853,
+  });
+
+  const [destination, setDestination] = React.useState ({
+    latitude: 33.640411,
+    longitude: -84.419853,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04
+        }}
+      >
+        <Marker 
+          draggable 
+          coordinate={origin}
+          onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+        />
+
+        <Marker 
+          draggable
+          coordinate={destination}
+          onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
+        />
+
+        <MapViewDirections 
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_KEY}
+        />
+
+        <Polyline 
+          coordinates={[ origin, destination ]}
+          strokeColor="pink"
+          strokeWidth={9}
+        />
+      </MapView>
     </View>
   );
 }
@@ -18,4 +62,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
+
 });
